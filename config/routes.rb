c9593b00b7e2e045e1d11fc3090a610bbe27d04c
daniel25994 <alex.tufa94@gmail.com>
2017-05-02 23:44:20 +0200

    Creates user model with devise and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Static pages
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/help'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+	
+  resources :users, :only => [:show]
 end
