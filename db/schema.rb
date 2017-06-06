ab87c2ead6bdab484de4a848cfca6a38bf35f15b
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2017-06-06 16:44:59 +0200

    Fixed student about me controller name

--- db/schema.rb
+++ db/schema.rb
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20170513133542) do
 
   create_table "users", force: :cascade do |t|
     t.string   "type"
+    t.string   "utype"
     t.string   "firstname"
     t.string   "lastname"
     t.string   "email"
