d635deaae5e82ddb86afde1baa98656c32198791
daniel25994 <alex.tufa94@gmail.com>
2017-06-07 00:45:57 +0200

    Started working on rspec test -basic one for user using Faker and FactoryGirl and start working on controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
 
 RSpec.describe UsersController, type: :controller do
+
+	describe "GET #show" do
+		test_user = FactoryGirl.create(:user)
+		before(:each) { get :show, params: {id: 1} }
+
+		it { should respond_with 200 }
+	end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,24 @@
 require 'spec_helper'
 
 RSpec.describe User, type: :model do
+
+  it "has a valid factory" do
+    expect(FactoryGirl.create(:user)).to be_valid
+  end
+
+  it "is invalid without a firstname" do
+    expect(FactoryGirl.build(:user,firstname: nil)).not_to be_valid
+  end
+
+  it "does not allow users with email different than email_confirmation" do
+    expect(FactoryGirl.build(:user,email: "a@a.it", email_confirmation: "a@b.it")).not_to be_valid
+  end
+
+  it "does not allow users with password different than password_confirmation" do
+    expect(FactoryGirl.build(:user,password: "Pass123", password_confirmation: "Pass23")).not_to be_valid
+  end
+=begin
+
   subject {
     described_class.new(firstname: "Pippo", email: "pi@pi.it",
                         password: "foobar", password_confirmation: "foobar")
@@ -29,4 +47,6 @@ RSpec.describe User, type: :model do
     subject.firstname = "a" * 255 + "@example.com"
     expect(subject).to_not be_valid
   end
+
+=end
 end
