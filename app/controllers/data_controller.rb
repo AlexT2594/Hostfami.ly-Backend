f801f51967674960d75b801a17d67b4867c36d4a
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2017-06-07 16:45:50 +0200

    Updated data controller

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -5,10 +5,12 @@ class DataController < ApplicationController
     if @current_user.student?
       render json: {
         user: @current_user,
-        educational_background: @current_user.educational_background,
-        health_lifestyle: @current_user.health_lifestyle,
-        my_description: @current_user.my_description,
-        passport_info: @current_user.passport_info
+        data: {
+          educational_background: @current_user.educational_background,
+          health_lifestyle: @current_user.health_lifestyle,
+          my_description: @current_user.my_description,
+          passport_info: @current_user.passport_info
+        }
       }
     elsif @current_user.family?
       render json: {
