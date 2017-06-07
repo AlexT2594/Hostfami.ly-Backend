5a84959339f31f52eab4e5214fdbdf8e1ec91008
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2017-06-07 14:35:46 +0200

    Forgotten data controller

---
+++ app/controllers/data_controller.rb
@@ -0,0 +1,24 @@
+class DataController < ApplicationController
+  before_filter :authenticate_request!
+
+  def show
+    if @current_user.student?
+      render json: {
+        user: @current_user,
+        educational_background: @current_user.educational_background,
+        health_lifestyle: @current_user.health_lifestyle,
+        my_description: @current_user.my_description,
+        passport_info: @current_user.passport_info
+      }
+    elsif @current_user.family?
+      render json: {
+        user: @current_user
+      }
+    else
+      render json: {
+        user: @current_user
+      }
+    end
+  end
+
+end
