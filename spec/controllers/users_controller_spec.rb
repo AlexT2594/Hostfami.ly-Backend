99b616ffd61ad35d632d43a3e3102657e6a5c8c8
Alex <alex.tufa94@gmail.com>
2017-06-07 19:19:03 +0200

    Working on rspec controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,8 +3,14 @@ require 'spec_helper'
 RSpec.describe UsersController, type: :controller do
 
 	describe "GET #show" do
-		test_user = FactoryGirl.create(:user)
+		before(:each){ @test_user=FactoryGirl.create(:student)}
 		before(:each) { get :show, params: {id: 1} }
+
+		it "returns the informations of the user with id=1" do
+			print @test_user.to_json
+			print json["user"]["type"]
+			expect(json).to have_key('user')
+		end
 
 		it { should respond_with 200 }
 	end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,20 +1,21 @@
 require 'faker'
 
 FactoryGirl.define do
-  factory :user do |f|
-    f.type "student"
-    f.firstname { Faker::Name.first_name }
-    f.lastname { Faker::Name.last_name }
-    f.email "a@a.it"
-    f.email_confirmation "a@a.it"
-    f.birthday { Faker::Date.birthday(10,100) }
-    f.state { Faker::Address.state }
-    f.city { Faker::Address.city }
-    f.address { Faker::Address.street_address }
-    f.password "Miao123"
-    f.password_confirmation "Miao123"
-    f.email_confirmed true
-    f.confirm_token { Faker::Crypto.md5 }
+  factory :user do |user|
+    factory :student,class: Student, parent: :user do |f|
+      f.firstname { Faker::Name.first_name }
+      f.lastname { Faker::Name.last_name }
+      f.email "a@a.it"
+      f.email_confirmation "a@a.it"
+      f.birthday { Faker::Date.birthday(10,100) }
+      f.state { Faker::Address.state }
+      f.city { Faker::Address.city }
+      f.address { Faker::Address.street_address }
+      f.password "Miao123"
+      f.password_confirmation "Miao123"
+      f.email_confirmed true
+      f.confirm_token { Faker::Crypto.md5 } 
+    end 
   end
 
   factory :invalid_user, parent: :user do |f|
