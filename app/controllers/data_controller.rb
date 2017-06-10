fe7d6edf0a9b31519fb3b291b7a75c8804a12ca8
Anxhelo Xhebraj <angelogebrai@gmail.com>
2017-06-10 02:13:22 +0200

    fixed user type

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -4,14 +4,14 @@ class DataController < ApplicationController
   def show
     if @current_user.student?
       render json: {
-        user: @current_user,
+        user: ActiveModelSerializers::SerializableResource.new(@current_user, adapter: :json).as_json,
         data: {
           about_me: @current_user.about_me,
           educational_background: @current_user.educational_background,
           health_lifestyle: @current_user.health_lifestyle,
           my_description: @current_user.my_description,
           passport_info: @current_user.passport_info
-        } 
+        }
       } , :except => [:created_at,:updated_at,:password_digest,:email_confirmed,:confirm_token]
     elsif @current_user.family?
       render json: {
