97b543c9b80d5e26c70186ce24fd54e8dbcf4c94
daniel25994 <alex.tufa94@gmail.com>
2017-06-15 22:59:39 +0200

    Fixed errors

--- app/models/student/about_me.rb
+++ app/models/student/about_me.rb
@@ -7,12 +7,12 @@ class Student::AboutMe < ApplicationRecord
   VALID_WEIGHT_REGEX = /^\d+(\.\d{1,2})?$/
   VALID_HEIGHT_REGEX = /^\d{2,3}$/
 
-  validates :tell, presence: true, length: {minimum: 4, maximum:15},
+  validates :tell, length: {minimum: 4, maximum:15},
   				   format: {with: VALID_TEL_REGEX}
-  validates :cell, presence: true, length: {minimum: 4,maximum: 15},
+  validates :cell, length: {minimum: 4,maximum: 15},
   				   format: {with: VALID_CELL_REGEX}
-  validates :religion, presence: true, length: {minimum: 2,maximum: 50}
-  validates :religion_celebrations, presence: true, :inclusione => 1..3
-  validates :height, presence: true, format: {with: VALID_HEIGHT_REGEX}
-  validates :weight, presence: true, format: {with: VALID_WEIGHT_REGEX}
+  validates :religion, length: {minimum: 2,maximum: 50}
+  validates :religion_celebrations, :inclusion => 1..3
+  validates :height, format: {with: VALID_HEIGHT_REGEX}
+  validates :weight, format: {with: VALID_WEIGHT_REGEX}
 end
