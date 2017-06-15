254a516d1609b675ec796e95ac791a06845e4a8d
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2017-06-15 13:01:53 +0200

    Updated data controller

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -5,24 +5,19 @@ class DataController < ApplicationController
     if @current_user.student?
       render json: {
         data: {
-          about_me: @current_user.about_me,
-          educational_background: @current_user.educational_background,
-          health_lifestyle: @current_user.health_lifestyle,
-          my_description: @current_user.my_description,
-          passport_info: @current_user.passport_info,
           uncompleted_sections: @current_user.uncompleted_sections
         }
       } , :except => [:created_at,:updated_at,:password_digest,:email_confirmed,:confirm_token]
     elsif @current_user.family?
       render json: {
         data: {
-          # Please render family forms here as student
+          uncompleted_sections: @current_user.uncompleted_sections
         }
       }
     else
       render json: {
         data: {
-          # Please render volunteer forms here as student
+          uncompleted_sections: []
         }
       }
     end
