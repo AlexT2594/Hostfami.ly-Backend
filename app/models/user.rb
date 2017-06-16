1364e13d9d96c7afa13164b1ddad23774f7f2566
Alex <alex.tufa94@gmail.com>
2017-06-16 11:04:51 +0200

    Date validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_PASSWORD_REGEX = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/
-  #VALID_DATE_REGEX = /\A(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})\z/
+  VALID_DATE_REGEX = /\A(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})\z/
 
   validates :firstname, presence: true, length: { minimum: 2, maximum: 50 }
   validates :lastname, presence: true, length: { minimum: 2, maximum: 50 }
@@ -60,7 +60,7 @@ class User < ApplicationRecord
   end
 
   def date_validation
-    if self.birthday and !Date.valid_date?(self.birthday.to_date.year,self.birthday.to_date.month,self.birthday.to_date.day)
+    if self.birthday and !self.birthday.match(VALID_DATE_REGEX)
       errors[:base] << "Invalid birthday"
     end
   end
